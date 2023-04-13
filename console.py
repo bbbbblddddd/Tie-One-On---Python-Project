f6a1e71ab262f0de84e43242ae9e0167fa7ea398
Brian Dickson <brianlawrencedickson@gmail.com>
2023-04-13 17:50:22 +0100

    much updated styling

--- console.py
+++ console.py
@@ -12,41 +12,67 @@ ingredient_repository.delete_all()
 cocktail_ingredient_repository.delete_all()
 
 gimlet = Cocktail('Gimlet', 'A lime-heavy, classic gin cocktail with a rich naval history and a sharp kick.',
-                  'Add the 60 ml of gin, 30ml of lime juice and 30ml of sugar syrup to a shaker with ice and shake until well-chilled. Strain into a chilled cocktail glass or an rocks glass filled with fresh ice. Garnish with a lime wheel.', 'https://i.imgur.com/lIekaw4.jpg')
+                  'Add the 60 ml of gin, 30ml of lime juice and 30ml of sugar syrup to a shaker with ice and shake until well-chilled. Strain into a chilled cocktail glass or an rocks glass filled with fresh ice. Garnish with a lime wheel.', 'https://i.imgur.com/iFmyUdo.png')
 cocktail_repository.save(gimlet)
 
-margerita = Cocktail('Margerita', 'Combining the tang of lime and the sweetness of orange liqueur with the distinctive strength of tequila.', 'Add 60ml of Tequila, 15ml of Triple Sec, 30ml of Lime Juice and 15ml of Sugar Syrup to a cocktail shaker filled with ice, and shake until well-chilled. Strain into a rocks glass over fresh ice. Garnish with a lime wheel and kosher salt rim or Tajin rim.')
+margerita = Cocktail('Margerita', 'Combining the tang of lime and the sweetness of orange liqueur with the distinctive strength of tequila.',
+                     'Add 60ml of Tequila, 15ml of Triple Sec, 30ml of Lime Juice and 15ml of Sugar Syrup to a cocktail shaker filled with ice, and shake until well-chilled. Strain into a rocks glass over fresh ice. Garnish with a lime wheel and kosher salt rim or Tajin rim.', 'https://i.imgur.com/hemYzqt.png')
 cocktail_repository.save(margerita)
 
-manhattan = Cocktail('Manhattan', 'The Manhattan is a classic cocktail of choice for whiskey-lovers.', 'Add the 60ml of Bourbon, 30ml Vermouth, and 2-3 dashes of Angustura Bitters into a mixing glass with ice and stir until well-chilled. Strain into a chilled Nick & Nora or Coupe glass. Garnish with a brandied cherry')
+manhattan = Cocktail('Manhattan', 'The Manhattan is a classic cocktail of choice for whiskey-lovers.',
+                     'Add the 60ml of Bourbon, 30ml Vermouth, and 2-3 dashes of Angustura Bitters into a mixing glass with ice and stir until well-chilled. Strain into a chilled Nick & Nora or Coupe glass. Garnish with a brandied cherry', 'https://i.imgur.com/3kB2NLr.png')
 cocktail_repository.save(manhattan)
 
-# could I rename these variable say... cocktail_gimlet, cocktail_margerita etc to make it more readable? Is this advised?
-# also what was the input in ordet to take a line during a string? My description TEXT field's are v.lomg and it makees it hard to navigate through + edit
+# whisky_sour = Cocktail(
+#     'Whisky Sour', 'A smooth cocktail with a hint of citrus sourness and an invigorating blast of whisky',  )
+
+eastside_gimlet = Cocktail('East Side Gimlet', 'A riff on the "Southside" cocktail that incorporates cucmumber aswell as mint',
+                           'Muddle 2 thick cucumber slices and 6 - 8 mint leaves in base of shaker. Add 60ml gin, 22.5ml lime juice, 22.5ml sugar syrup, Shake with ice and fine strain into chilled glass. Garnish with a mint leaf')
+cocktail_repository.save(eastside_gimlet)
+
 
 gin = Ingredient ('Gin')
 ingredient_repository.save(gin)
 
-lime_juice = Ingredient ('Lime Juice')
-ingredient_repository.save(lime_juice)
-
-sugar_syrup = Ingredient ('Sugar Syrup')
-ingredient_repository.save(sugar_syrup)
-
-tequila = Ingredient ('Tequila')
+tequila = Ingredient('Tequila')
 ingredient_repository.save(tequila)
 
-triple_sec = Ingredient ('Triple Sec')
-ingredient_repository.save(triple_sec)
-
-bourbon = Ingredient ('Bourbon')
+bourbon = Ingredient('Bourbon')
 ingredient_repository.save(bourbon)
 
-vermouth = Ingredient ('Vermouth')
+whisky = Ingredient('Whisky')
+ingredient_repository.save(whisky)
+
+rum = Ingredient('Rum')
+ingredient_repository.save(rum)
+
+vermouth = Ingredient('Vermouth')
 ingredient_repository.save(vermouth)
+
+lime_juice = Ingredient('Lime Juice')
+ingredient_repository.save(lime_juice)
+
+triple_sec = Ingredient('Triple Sec')
+ingredient_repository.save(triple_sec)
+
+lemon_juice = Ingredient('Lemon Juice')
+ingredient_repository.save(lemon_juice)
+
+sugar_syrup = Ingredient('Sugar Syrup')
+ingredient_repository.save(sugar_syrup)
 
 angustura_bitters = Ingredient ('Angustura Bitters')
 ingredient_repository.save(angustura_bitters)
+
+cucumber = Ingredient('Cucumber Slices')
+ingredient_repository.save(cucumber)
+
+mint = Ingredient('Mint Leaves')
+ingredient_repository.save(mint)
+
+egg = Ingredient('Egg White')
+ingredient_repository.save(egg)
+
 
 
 
@@ -63,5 +89,11 @@ cocktail_ingredient_repository.save(sugar_syrup.id, margerita.id)
 cocktail_ingredient_repository.save(bourbon.id, manhattan.id)
 cocktail_ingredient_repository.save(vermouth.id, manhattan.id)
 cocktail_ingredient_repository.save(angustura_bitters.id, manhattan.id)
+
+cocktail_ingredient_repository.save(gin.id, eastside_gimlet.id)
+cocktail_ingredient_repository.save(lime_juice.id, eastside_gimlet.id)
+cocktail_ingredient_repository.save(sugar_syrup.id, eastside_gimlet.id)
+cocktail_ingredient_repository.save(cucumber.id, eastside_gimlet.id)
+cocktail_ingredient_repository.save(mint.id, eastside_gimlet.id)
 
 pdb.set_trace()
